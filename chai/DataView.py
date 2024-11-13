7704ef7ac1aa49429d0339ac0785ac5238413add
Martin Hierholzer <martin.hierholzer@desy.de>
2024-11-13 15:08:09 +0100

    feat: channel selector

--- chai/DataView.py
+++ chai/DataView.py
@@ -3,8 +3,11 @@ from textual.screen import ModalScreen
 from textual.containers import Horizontal, Vertical, Container, Grid
 from textual.widgets import Button, Label, Static, Input, Button, DataTable, Input
 from textual.containers import ScrollableContainer
+from textual.validation import Number
 
 from textual import on, events
+
+from chai import Utils
 
 import deviceaccess as da
 
@@ -86,16 +89,27 @@ class EditValueScreen(ModalScreen):
 
 class RegisterValueField(ScrollableContainer):
 
-    _register: da.NumpyGeneralRegisterAccessor | None = None
+    _register: da.TwoDRegisterAccessor | None = None
     _channel: int = 0
     _isRaw : bool = False
+
+    def compose(self) -> ComposeResult :
+        table = DataTable()
+        table.add_columns('Value', 'Raw (dec)', 'Raw (hex)')
+        yield table
 
     def changeRegister(self, register: da.TwoDRegisterAccessor):
         self._register = register
         self._isRaw = da.AccessMode.raw in self._register.getAccessModeFlags()
         self._register.readLatest()
+        self._channel = 0
         self.update()
 
+    def changeChannel(self, channel: int):
+        if self._register is None:
+            return
+        self._channel = channel
+        self.update()
 
     def on_key(self, event: events.Key) -> None:
         if event.key != 'enter':
@@ -128,9 +142,8 @@ class RegisterValueField(ScrollableContainer):
             table.update_cell_at(coordinate=[row,0], value=str(self._register[self._channel][row]), update_width=True)
 
     def update(self) -> None:
-        self.remove_children()
-
-        table = DataTable()
+        table = self.query_one(DataTable)
+        table.clear(True)
 
         if self._isRaw :
             table.add_columns('Value', 'Raw (dec)', 'Raw (hex)')
@@ -141,34 +154,72 @@ class RegisterValueField(ScrollableContainer):
             for element, value in enumerate(self._register[self._channel]):
                 table.add_row(value, label=str(element))
 
-        self.mount(table)
 
 
-class DataView(ScrollableContainer):
+class RegisterInfo(Grid):
+    _nChannels: int = 0
+
+    def compose(slef) -> ComposeResult:
+        yield Label("Register Path", id="label_register_path")
+        yield Static(" ", id="field_register_path")
+        yield Label("Dimension")
+        yield Static("", id="label_dimensions")
+        yield Label("nElements")
+        yield Static("", id="label_nELements")
+        yield Label("nChannels")
+        yield Static("", id="label_nChannels")
+        yield Label("Data Type")
+        yield Static("", id="label_data_type")
+        yield Label("wait_for_new_data")
+        yield Static("", id="label_wait_for_new_data")
+        yield Label("Channel:")
+        yield Input(value="0", placeholder="0", id="edit_value_dialog_input", type="integer")
+
+    def changeRegister(self, registerInfo) -> None:
+        self.query_one("#field_register_path").update(registerInfo.getRegisterName())
+        self.query_one("#label_nELements").update(str(registerInfo.getNumberOfElements()))
+        self.query_one("#label_nChannels").update(str(registerInfo.getNumberOfChannels()))
+        self._nChannels = registerInfo.getNumberOfChannels()
+
+        wait_for_new_data_label_text = "no"
+        cont_poll_text = "Continous Poll"
+        freq_text = "Poll frequency"
+
+        if da.AccessMode.wait_for_new_data in registerInfo.getSupportedAccessModes():
+            wait_for_new_data_label_text = "yes"
+            cont_poll_text = "Continous Read"
+            freq_text = "Update frequency"
+
+        self.app.query_one("#label_poll_update_frq").update(freq_text)
+        self.app.query_one("#label_ctn_pollread").update(cont_poll_text)
+        self.query_one("#label_wait_for_new_data").update(wait_for_new_data_label_text)
+
+        dd = registerInfo.getDataDescriptor()
+        self.query_one("#label_data_type").update(Utils.build_data_type_string(dd))
+        if registerInfo.getNumberOfDimensions() == 0:
+            self.query_one("#label_dimensions").update("Scalar")
+        elif registerInfo.getNumberOfDimensions() == 1:
+            self.query_one("#label_dimensions").update("1D")
+        elif registerInfo.getNumberOfDimensions() == 2:
+            self.query_one("#label_dimensions").update("2D")
+
+    def on_input_submitted(self, change: Input.Submitted) -> None :
+        if change.value.isdigit() :
+            value = int(change.value)
+        else :
+            # might be empty...
+            value = 0
+        if value >= self._nChannels:
+            value = self._nChannels-1
+            change.input.value = str(value)
+            self.notify("Channel out of range.", severity="warning")
+        self.app.query_one(RegisterValueField).changeChannel(value)
+
+
+class DataView(Vertical):
+
     def compose(self) -> ComposeResult:
-        yield Vertical(
-            Label("Register Path"),
-            Static("", id="label_register_path"),
-            Horizontal(
-                Vertical(
-                    Label("Dimension"),
-                    Static("", id="label_dimensions")
-                ),
-                Vertical(
-                    Label("nElements"),
-                    Static("", id="label_nELements")
-                ),
-            ),
-            Horizontal(
-                Vertical(
-                    Label("Data Type"),
-                    Static("", id="label_data_type")
-                ),
-                Vertical(
-                    Label("wait_for_new_data"),
-                    Static("", id="label_wait_for_new_data")
-                ),
-            ),
-            RegisterValueField(),
-            id="properties",
-            classes="main_col")
+        self.add_class("main_col")
+
+        yield RegisterInfo(id="register_info")
+        yield RegisterValueField(id="register_value_table")
