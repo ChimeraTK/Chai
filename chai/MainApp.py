98b349bdf5f2b8eb19cd2a84237ba1163adf1ad3
Christian Willner <34183939+vaeng@users.noreply.github.com>
2025-09-09 16:15:52 +0200

    feat: intial footer with bindings

--- chai/MainApp.py
+++ chai/MainApp.py
@@ -2,6 +2,8 @@ from textual.app import App, ComposeResult
 from textual.screen import Screen
 from textual.containers import Horizontal, Container
 from textual.widgets import Header, Footer
+from textual.binding import Binding
+from textual import log
 
 from textual.css import query
 from textual.widget import Widget
@@ -31,11 +33,47 @@ class MainScreen(Screen):
     CSS_PATH = "Chai.tcss"
     TITLE = "Console Hardware Interface"
     SUB_TITLE = f"@ {socket.gethostname()}"
+    BINDINGS = [
+        # TODO: seperate bindings from displayed text, so that it is not removed when key is bind by another action in some field. Or give priority to the main screen bindings
+        Binding(key="ctrl+m", key_display="^m", tooltip="Load dmap file", action="showDmapScreen", description="dmap"), Binding(
+            key="ctrl+d", key_display="^d", tooltip="Select Device", action="showDeviceScreen", description="dev"),
+        Binding(
+            key="ctrl+i", key_display="^i", tooltip="Show Device Properties", action="showPropertiesScreen", description="devInfo"),
+        Binding(
+            key="ctrl+r", key_display="^r", tooltip="Show Register Tree", action="showRegisterScreen", description="reg"),
+        Binding(
+            key="ctrl+e", key_display="^e", tooltip="Show Register Meta Data", action="showMetaDataScreen", description="meta"),
+        Binding(
+            key="ctrl+a", key_display="^a", tooltip="Show Register Content", action="showContentScreen", description="content"),
+        Binding(
+            key="ctrl+o", key_display="^o", tooltip="Show Options", action="showOptionsScreen", description="opts"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield ConsoleHardwareInterface()
         yield Footer()
+
+    def _action_showDmapScreen(self) -> None:
+        log("dmap load")
+
+    def _action_showDeviceScreen(self) -> None:
+        log("device screen")
+
+    def _action_showPropertiesScreen(self) -> None:
+        log("properties screen")
+
+    def _action_showRegisterScreen(self) -> None:
+        log("register screen")
+
+    def _action_showMetaDataScreen(self) -> None:
+        log("metadata screen")
+
+    def _action_showContentScreen(self) -> None:
+        log("content screen")
+
+    def _action_showOptionsScreen(self) -> None:
+        log("options screen")
 
 
 class LayoutApp(App):
