aa7b29e00168babcaabdccb2a4774361f4237f44
Martin Hierholzer <martin.hierholzer@desy.de>
2025-09-10 11:53:08 +0200

    fix: crash if dmap file does not exist

--- Chai.py
+++ Chai.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 from chai.MainApp import LayoutApp
+import sys
+import os
 
 if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        if not os.path.isfile(sys.argv[1]):
+            print(f'Error: File "{sys.argv[1]}" does not exist!')
+            sys.exit(1)
+
     app = LayoutApp()
     app.run()
